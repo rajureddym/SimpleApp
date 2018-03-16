a73c4841f15d91b7e37f99ab2af526187a9035a7
root <root@ip-172-31-44-135.ec2.internal>
2018-03-16 15:32:33 +0000

    added some new files

---
+++ src/main/java/com/marlabs/demo/Test.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.marlabs.demo;
+
+/**
+ * @author Nanda
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tested!");
+		System.out.println(new java.util.Date());
+		System.out.println("Changed!");
+		System.out.println("Krthik...");
+		System.out.println("added a new line by Shiv ");
+		//Trying to do some changes to test the merging 
+		System.out.println("testing the merge command cool");
+		System.out.print("working from eclipse");
+		System.out.println("Eron");
+		System.out.println("By Kunneng");
+	}
+
+}
